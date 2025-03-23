31b8984e605f60cd7389cfd5d9a0b4376a8924aa
Mark-Ameka <xenonmendozaxenon@gmail.com>
2025-03-23 20:41:20 +0800

    fixed app.jsx route handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,48 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 import Home from "./pages/Home.jsx";
 import UpdateProfile from "./pages/UpdateProfile.jsx";
 import ViewBook from "./pages/books/ViewBook.jsx";
 import ChangeBook from "./pages/books/ChangeBook.jsx";
-import { useSelector } from "react-redux";
+
+// Higher-Order Component for protected routes
+const ProtectedRoute = ({ token, children, redirectTo }) => {
+  return token ? children : <Navigate to={redirectTo} />;
+};
 
 const App = () => {
   const { token } = useSelector((state) => state.auth);
 
+  const routes = [
+    { path: "/home", element: <Home />, protected: true },
+    { path: "/login", element: <Login />, protected: false },
+    { path: "/register", element: <Register />, protected: false },
+    { path: "/update-profile", element: <UpdateProfile />, protected: true },
+    { path: "/books/:id", element: <ViewBook />, protected: true },
+    { path: "/update-book/:id", element: <ChangeBook />, protected: true },
+  ];
+
   return (
     <Routes>
-      <Route
-        path="/home"
-        element={token ? <Home /> : <Navigate to="/login" />}
-      />
-      <Route
-        path="/login"
-        element={!token ? <Login /> : <Navigate to="/home" />}
-      />
-      <Route
-        path="/register"
-        element={!token ? <Register /> : <Navigate to="/home" />}
-      />
-      <Route
-        path="/update-profile"
-        element={token ? <UpdateProfile /> : <Navigate to="/login" />}
-      />
-
-      {/* books */}
-
-      <Route
-        path="/books/:id"
-        element={token ? <ViewBook /> : <Navigate to="/login" />}
-      />
-
-      <Route
-        path="/update-book/:id"
-        element={token ? <ChangeBook /> : <Navigate to="/login" />}
-      />
+      {routes.map(({ path, element, protected: diquwbd }) => (
+        <Route
+          key={path}
+          path={path}
+          element={
+            diquwbd ? (
+              <ProtectedRoute token={token} redirectTo="/login">
+                {element}
+              </ProtectedRoute>
+            ) : (
+              <ProtectedRoute token={!token} redirectTo="/home">
+                {element}
+              </ProtectedRoute>
+            )
+          }
+        />
+      ))}
     </Routes>
   );
 };
